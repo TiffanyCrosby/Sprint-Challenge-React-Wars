22df71ae08ba5ca6909e24fbd76b6568e61b6cd5
Tiffany Crosby <tiffany.crosby65@gmail.com>
2020-02-21 16:47:38 -0500

    added img to cards.

--- starwars/src/components/Cards.js
+++ starwars/src/components/Cards.js
@@ -1,9 +1,16 @@
 import React, { useState } from 'react';
 import { Col, Card, CardImg, CardText, CardBody, CardTitle, Button } from 'reactstrap';
 import styled from 'styled-components';
+import StarWarsImg from './StarWars Cast.jpg'
 
 const StyledDiv = styled.div`
     margin: 0 auto;
+`;
+
+const StyledImg = styled.img`
+    width: 20vw;
+    height: 25vh;
+    border: 3px double black;
 `;
 
 const Cards = props => {
@@ -15,8 +22,7 @@ const Cards = props => {
                 <Card>
                     <CardBody>
                         <CardTitle>{props.name}</CardTitle>
-                        {/* <CardImg alt='StarWars Cast' src='./StarWars Cast' /> */}
-                        {/* <img alt='StarWars Cast' src='./'/> */}
+                        <StyledImg alt='StarWars Cast' src={StarWarsImg} />
                         <CardText> Height: {props.height}</CardText>
                         <CardText>Weight: {props.weight}</CardText>
                         <Button onClick={() => { setLikes(likes + 1) }}>{`How many times I've been liked: ${likes}`}</Button>
